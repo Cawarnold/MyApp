74621ac2b23feed6c94905ba8c752d67e403a328
Cawarnold <christian.w.arnold@gmail.com>
2015-04-20 14:31:25 +0000

    first commit

---
+++ MyApp_Site.md
@@ -0,0 +1,15 @@
+My App
+
+https://ecostuff-cawarnold.c9.io
+
+#### Admin Site 
+
+http://ecostuff-cawarnold.c9.io/admin/
+
+cawarnold / H...
+
+#### Polls Site
+
+http://ecostuff-cawarnold.c9.io/polls/
+
+
---
+++ ecostuff/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+
+#urlpatterns = patterns('',
+#    # Examples:
+#    # url(r'^$', 'ecostuff.views.home', name='home'),
+#    # url(r'^blog/', include('blog.urls')),
+#
+#    url(r'^admin/', include(admin.site.urls)),
+#)
