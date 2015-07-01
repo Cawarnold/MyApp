9ec693b194fb24f164b3d6dca76cf7fe8fc70ee8
Cawarnold <christian.w.arnold@gmail.com>
2015-07-01 17:23:07 +0100

    made some changes to the app's DB. Re migrated the DB

---
+++ polls/migrations/0002_auto_20150701_1718.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='question',
+            name='pub_date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 7, 1, 16, 18, 4, 961000, tzinfo=utc), verbose_name=b'date published'),
+        ),
+    ]
