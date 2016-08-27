5fb974ca5834982b1f95830aa849571138daf6a9
Cawarnold <christian.w.arnold@gmail.com>
2016-08-27 08:10:11 +0100

    CA_TR: More cleaning to look like polls but without url/polls

--- ecostuff/urls.py
+++ ecostuff/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from ecostuff.home import views
 
 urlpatterns = patterns('',
-	url(r'^$', 'home.views.index')
+	url(r'^$', include('home.urls', namespace="home"))
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
--- home/views.py
+++ home/views.py
@@ -7,17 +7,4 @@ from django.utils import timezone
 
 
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """
-        Return the last 5 published questions (not including those set to be
-        published in the future).
-        """
-        #return Question.objects.filter(
-        #    pub_date__lte=timezone.now()
-        #).order_by('-pub_date')[:5]
-
-        return [i for i in Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5] if i.choice_set.exists()]
-
+    template_name = 'index.html'
